f80ebc028a8978d3ed9fe94f29c1098d504d0c0e
NoelTheGnom <noeljarling@gmail.com>
2024-12-02 21:01:54 +0100

    8 current   2024-12-02 20:41:49  24.11.20241130.62c435d   6.6.63                          *

---
+++ nixos-rebuild.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e # Exit on non-zero status
+
+# cd into config dir
+pushd /etc/nixos
+
+# Shows your changes
+git diff -U0
+
+echo "NixOS Rebuilding..."
+
+# Rebuild, output simplified errors, log trackebacks
+sudo nixos-rebuild switch --flake path:/etc/nixos#default || (cat nixos-switch.log | grep --color error && exit 1)
+
+# Get current generation metadata
+current=$(nixos-rebuild list-generations | grep current)
+
+# Commit all changes witih the generation metadata
+git commit -am "$current"
+
+# Back to where you were
+popd
+
+# Notify all OK!
+notify-send -e "NixOS Rebuilt OK!" --icon=software-update-available
