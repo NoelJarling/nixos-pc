dd9cbc8213bc55bfdcfa782ddd088af73a9f0525
NoelTheGnom <noeljarling@gmail.com>
2024-12-02 21:15:21 +0100

    8 current   2024-12-02 20:41:49  24.11.20241130.62c435d   6.6.63                          *

--- nixos-rebuild.sh
+++ nixos-rebuild.sh
@@ -10,7 +10,7 @@ git diff -U0
 echo "NixOS Rebuilding..."
 
 # Rebuild, output simplified errors, log trackebacks
-sudo nixos-rebuild switch --flake path:/etc/nixos#default &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+sudo nixos-rebuild switch --flake path:/etc/nixos#default &>log/nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
 
 # Get current generation metadata
 current=$(nixos-rebuild list-generations | grep current)
