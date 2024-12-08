9a69711c7e0f0d12755898128731e5fa354a2005
NoelTheGnom <noeljarling@gmail.com>
2024-12-08 15:00:10 +0100

    42 current  2024-12-08 14:58:38  24.11.20241130.62c435d  6.6.63                          *

--- configuration.nix
+++ configuration.nix
@@ -178,6 +178,7 @@
     winetricks
     wineWowPackages.stable
     bind
+    psmisc
   ];
   environment.sessionVariables.VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
   fonts.packages = with pkgs; [
