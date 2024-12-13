e28b3611790ec1f7f2864e7cc98190a0bae743bf
NoelTheGnom <noeljarling@gmail.com>
2024-12-13 12:47:20 +0100

    52 current  2024-12-13 12:47:19  24.11.20241130.62c435d  6.6.63                          *

--- hyprland.nix
+++ hyprland.nix
@@ -49,7 +49,8 @@
                     "$mainMod ALT, l, movewindow, r"
                     "$mainMod ALT, i, movewindow, u"
                     "$mainMod ALT, k, movewindow, d"
-                    ", code:123, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+                    ", code:123, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+                    ", code:122, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
                 ];
                 exec-once = [
                     "swaync"
