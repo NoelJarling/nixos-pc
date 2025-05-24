765a8a3eca3a11de40fcf9fc7a4196a1093c7398
NoelTheGnom <noeljarling@gmail.com>
2025-05-24 11:37:24 +0200

    Added disko config

--- nixos-server/flake.nix
+++ nixos-server/flake.nix
@@ -4,6 +4,9 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
+    disko.url = "github:nix-community/disko/latest";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+
     /*home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,6 +18,7 @@
       specialArgs = {inherit inputs;};
       modules = [
         ./configuration.nix
+        disko.nixosModules.disko
         #inputs.home-manager.nixosModules.default
       ];
     };
