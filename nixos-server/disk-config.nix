caa2f7ea621ead4284c28ed0814e2a7c860fd976
NoelTheGnom <noeljarling@gmail.com>
2025-05-24 09:59:58 +0200

    Added disko config

---
+++ nixos-server/disk-config.nix
@@ -0,0 +1,44 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100G";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+            media = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/media-data";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
