6af284873237b70da61726d0919e56221c460080
NoelTheGnom <noeljarling@gmail.com>
2025-07-03 15:31:27 +0200

    90 current  2025-07-03 15:31:26  24.11.20241130.62c435d  6.6.63                          *

--- nixos-pc/configuration.nix
+++ nixos-pc/configuration.nix
@@ -195,6 +195,7 @@
     jellyfin-media-player
     xow_dongle-firmware # Xbox one dongle firmware 
     google-chrome
+    hyprpaper
   ];
   environment.sessionVariables.VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
   fonts.packages = with pkgs; [
