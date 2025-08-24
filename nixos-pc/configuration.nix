00e1bb9b4673d2df84030b2faecdac08ad729af7
NoelTheGnom <noeljarling@gmail.com>
2025-08-24 20:21:20 +0200

    119 current  2025-08-24 20:21:18  25.05.20250823.b1b3291  6.12.43                          *

--- nixos-pc/configuration.nix
+++ nixos-pc/configuration.nix
@@ -201,7 +201,6 @@
     google-chrome
     gimp
     unstable.grayjay
-    mullvad-vpn
   ];
   environment.sessionVariables.VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
   fonts.packages = with pkgs; [
